c22a1b05ef055b55e24739b6e622faea930a8523
Élio Cró <elio.cro@gmail.com>
2015-11-16 22:11:11 +0000

    Display metadata

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var remote = require('remote');
+
 angular.module('quaver', [])
 
-.controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope, $timeout) {
   var buffer, player;
   var fileManager = remote.require('./lib/file-manager');
 
@@ -13,6 +15,28 @@ angular.module('quaver', [])
 
   $scope.play = function  () {
     player = player || AV.Player.fromBuffer(buffer);
+
+
+    player.on('duration', function (duration) {
+      console.log('duration', arguments);
+      $timeout(function () {
+        $scope.duration = msToTime(duration);
+      });
+    });
+
+    player.on('metadata', function (metadata) {
+      console.log('metadata', metadata);
+      $timeout(function () {
+        $scope.metadata = metadata;
+      });
+    });
+
+    player.on('progress', function (progress) {
+      $timeout(function () {
+        $scope.progress = msToTime(progress);
+      });
+    });
+
     player.play();
   };
 
@@ -23,7 +47,18 @@ angular.module('quaver', [])
   $scope.stop = function  () {
     player.stop();
     player = null;
+    $scope.progress = '00:00';
   };
+
+
+  function msToTime (duration) {
+    var seconds = parseInt((duration/1000)%60);
+    var minutes = parseInt(duration/(1000*60));
+
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+    return minutes + ':' + seconds;
+  }
 })
 
 ;
